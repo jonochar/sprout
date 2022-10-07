d84d34378486689ebf7c2f05e809c28d44081609
jonochar <jonochar1@gmail.com>
2022-10-07 00:52:33 -0500

    Rearranged and added script directory

---
+++ scripts/read_fuses.ps1
@@ -0,0 +1,2 @@
+# avrdude -c atamel-ice -p m328p -B 25 -U lfuse:w:0xFF:m -U hfuse:w:0xDE:m -U efuse:w:0x05:m
+.\..\avrdude-v7.0-windows-windows-x64\avrdude.exe -c atmelice_isp -p m328p -B 25 -U lfuse:r:lfuse.hex:h -U hfuse:r:hfuse.hex:h -U efuse:r:efuse.hex:h
